8f8bbe8ff48e6cbaae92b42e314c52a99e776874
Irina Rogozhova <irina.rogozhova@saltpepper.ru>
2018-09-26 21:43:43 +0300

    initial

---
+++ script.js
@@ -0,0 +1,17 @@
+// var age = 17;
+// var drink = age >= 18 ? "beer" : "juice";
+// console.log("John drinks" + " " + drink);
+
+var job = 'instructor';
+
+switch (job) {
+  case 'teacher':
+  case 'instructor':
+    console.log("John teaches kids");
+    break;
+  case 'driver':
+    console.log("John drives a car");
+    break;
+  default:
+    console.log("John does something else");
+}
